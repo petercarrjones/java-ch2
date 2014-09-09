8dee02d5e3566d630ab15bee4ddf5aaea015ed67
Peter Jones <pjones15@gmu.edu>
2014-09-09 02:53:05 -0400

    Add 'buzz' to 'fizz'

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,1 @@
-for( var count=0; count <=100; count++) if (count % 3 == 0) console.log("Fizz"); else console.log(count);
+for( var count=0; count <=100; count++) if ((count % 3) == 0) console.log("Fizz"); else if ((count % 5) == 0) console.log("Buzz"); else console.log(count);
