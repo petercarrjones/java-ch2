ad0c7ace44a3a50d32f01bd8f4d083428205a243
Peter Carr Jones <Peter.carr.jones@gmail.com>
2014-09-10 19:09:56 -0400

    format program lines better
    Not in one line, almost unreadable.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,8 @@
-for( var count=0; count <=100; count++) if ((count % 3) == 0 && count % 5 == 0) console.log("FizzBuzz"); else if (count % 3 == 0) console.log("Fizz"); else if ((count % 5) == 0) console.log("Buzz"); else console.log(count);
+for( var count=0; count <=100; count++)
+if ((count % 3) == 0 && count % 5 == 0)
+    console.log("FizzBuzz"); 
+    else if (count % 3 == 0) 
+      console.log("Fizz"); 
+      else if ((count % 5) == 0) 
+      console.log("Buzz"); 
+      else console.log(count);
