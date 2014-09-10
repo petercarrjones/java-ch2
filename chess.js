2b5fed08b0d617780fed5657c1d828d0dab41e2f
Peter Jones <pjones15@gmu.edu>
2014-09-10 00:25:49 -0400

    checkmate

--- chess.js
+++ chess.js
@@ -1,7 +1,8 @@
-var chesswhite=" #"; chessblack="# ";  
-for {
-	( var count=0; count <=4; count++)
-	 console.log(chessblack + chessblack)
-	}
+var chesswhite=" # # # #"; chessblack="# # # # ";  
+for( var count=1; count <=8; count++)
+	if (count%2==0)
+	 console.log(chesswhite);
+	else console.log(chessblack);
+	
 
-for {( var count=0; count <=4; count++) console.log(chesswhite + chesswhite);} 
+
