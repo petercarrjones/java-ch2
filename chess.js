89fb0801a624b0a1eca55fc28972f3a245fa3e5f
Peter Jones <pjones15@gmu.edu>
2014-09-10 00:00:16 -0400

    Initial commit- make it work

---
+++ chess.js
@@ -0,0 +1,7 @@
+var chesswhite=" #"; chessblack="# ";  
+for {
+	( var count=0; count <=4; count++)
+	 console.log(chessblack + chessblack)
+	}
+
+for {( var count=0; count <=4; count++) console.log(chesswhite + chesswhite);} 
