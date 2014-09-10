dbd30eeb5bd08caca697acf3bdd9afbab8bd55da
Peter Jones <pjones15@gmu.edu>
2014-09-10 00:10:44 -0400

    Revert "Checkmate"
    This reverts commit e48dddc9542101d150d84afd7878a89e51155ca2.

--- chess.js
+++ chess.js
@@ -1,8 +1,7 @@
-var chesswhite=" # # # #"; chessblack="# # # # ";  
-for( var count=1; count <=8; count++)
-	if (count%2==0)
-	 console.log(chesswhite);
-	else console.log(chessblack);
-	
+var chesswhite=" #"; chessblack="# ";  
+for {
+	( var count=0; count <=4; count++)
+	 console.log(chessblack + chessblack)
+	}
 
-
+for {( var count=0; count <=4; count++) console.log(chesswhite + chesswhite);} 
